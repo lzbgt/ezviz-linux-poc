25edeb85374edbbb9d9d34f1b2f28af651445cb4
blu <lzbgt@126.com>
2019-05-30 09:47:23 +0000

    poc to alphaversion

---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+/*
+#cgo CFLAGS: -Ithirdparty/EZServerOpenSDK/inc
+#cgo LDFLAGS: -Lthirdparty/EZServerOpenSDK/lib/linux64 -lcrypto -lssl -lcurl -lprotobuf -lexpat -lhpr -lhlog -lopensslwrap -lys_net -lezviz_streamclient -lezserveropensdk
+#include <ESOpenStream.h>
+*/
+import "C"
+
+func main() {
+	
+}
