d30af8016e6192b9ae8260f3fb6bffd7adf5e774
blu <lzbgt@126.com>
2019-09-19 10:12:42 +0800

    big refacting of communitation architect

--- src/inc/svc.hpp
+++ src/inc/svc.hpp
@@ -493,6 +493,32 @@ private:
         return old;
     }
 
+    int RedisSAdd(string key, vector<string> val){
+        auto f_ = redisClient.sadd(key, val);
+        redisClient.sync_commit();
+        auto r_ = f_.get();
+        if(r_.is_integer()) {
+            return r_.as_integer();
+        }else{
+            return 0;
+        }
+    }
+
+    vector<string> RedisSMembers(string key) {
+        vector<string> ret;
+        auto get_ = redisClient.smembers(key);
+        redisClient.sync_commit();
+        auto r = get_.get();
+        if(r.is_array()) {
+            auto arr = r.as_array();
+            for(auto &i:arr) {
+                ret.push_back(i.as_string());
+            }
+        }
+        
+        return ret;
+    }
+
     void RedisDelete(string key)
     {
         // TODO: implement soft deletion?
@@ -733,7 +759,7 @@ private:
         }
 
 
-        if(EZCMD::RTPLAY != ezCmd||EZCMD::RTPLAY_CTN != ezCmd) {
+        if(EZCMD::RTPLAY != ezCmd && EZCMD::RTPLAY_CTN != ezCmd) {
             cerr << "\tinvalid messge " << endl;
         }
         else {
