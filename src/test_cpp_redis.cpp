b89b93fb880461c04c15fb48028504da30a6698f
blu <lzbgt@126.com>
2019-09-19 10:12:58 +0800

    big refacting of communitation architect

---
+++ src/test_cpp_redis.cpp
@@ -0,0 +1,62 @@
+#include <cpp_redis/cpp_redis>
+#include <iostream>
+
+using namespace std;
+
+static cpp_redis::client redisClient;
+
+int InitRedis()
+{
+    redisClient.connect("192.168.1.102", 6379, [](const std::string& host, std::size_t port, cpp_redis::client::connect_state status) {
+        if (status == cpp_redis::client::connect_state::dropped) {
+            std::cout << "client disconnected from " << host << ":" << port << std::endl;
+        }
+    }, 1000*2, -1, 1000*3);
+
+    redisClient.sync_commit();
+    return 0;
+}
+
+int RedisSAdd(string key, vector<string> val){
+    auto f_ = redisClient.sadd(key, val);
+    redisClient.sync_commit();
+    auto r_ = f_.get();
+    if(r_.is_integer()) {
+        return r_.as_integer();
+    }else{
+        return 0;
+    }
+}
+
+vector<string> RedisSMembers(string key) {
+    vector<string> ret;
+    auto get_ = redisClient.smembers(key);
+    redisClient.sync_commit();
+    auto r = get_.get();
+    if(r.is_array()) {
+        auto arr = r.as_array();
+        for(auto &i:arr) {
+            ret.push_back(i.as_string());
+        }
+    }
+
+    return ret;
+}
+
+int main(){
+    InitRedis();
+
+    string key = "test_sadd";
+    vector<string> val = {"1", "2", "3"};
+    int r = RedisSAdd(key, val);
+    if(r == 0) {
+        cout << "no change";
+    }
+
+    auto v = RedisSMembers(key);
+    for(auto &i:v) {
+        cout << i << ";";
+    }
+
+    return 0;
+}
